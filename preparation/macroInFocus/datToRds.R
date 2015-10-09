dc1a09df83edfd91b0a6bdf2f04ac14adb014c45
Julien MOEYS <julienmoeys@yahoo.fr>
2015-10-09 11:10:28 +0200

    MACROInFOCUS benchmark

---
+++ preparation/macroInFocus/datToRds.R
@@ -0,0 +1,35 @@
+
+#   This script is used to convert text tabular files 
+#   (themselves exported by MACROInFOCUS), into more compact 
+#   RDS files, that can then be used to check that the 
+#   differences between macroInFocusGWConc() and MACROInFOCUS 
+#   are very small (when calculating processing results)
+
+setwd( wd <- "D:/Users/julienm/Documents/_WORKS/_PROJECTS/r_packages/macro-sp/macroutils/preparation/macroInFocus" )
+
+outDir <- "D:/Users/julienm/Documents/_WORKS/_PROJECTS/r_packages/macro-sp/macroutils/pkg/macroutils/inst/bintest/macroInFocus"
+
+allDat <- list.files( wd, pattern = ".dat", ignore.case = TRUE )
+
+for( i in 1:length( allDat ) ){
+    #   i <- 1L
+    
+    #   Format the name of the output RDS file
+    rdsName <- allDat[ i ]
+    rdsName <- gsub( x = rdsName, pattern = ".dat", 
+        replacement = ".rds", ignore.case = TRUE )
+    
+    #   Import the txt file (of the converted bin files)
+    inTbl <- read.table(
+        file    = allDat[ i ], 
+        header  = TRUE, 
+        sep     = "", 
+        dec     = ".", 
+        strip.white = TRUE
+    )   
+    
+    #   Re-export it as RDS file
+    saveRDS( object = inTbl, file = file.path( outDir, rdsName ) )
+    
+    rm( inTbl, rdsName )
+}   
