4c3ee1211bfc8aa2af3f0e2996f2e23f834fc433
Julien MOEYS <julienmoeys@yahoo.fr>
2016-06-08 11:25:03 +0200

    Test if fixing encoding solve a problem on linux

--- pkg/macroutils/R/macroutils.R
+++ pkg/macroutils/R/macroutils.R
@@ -250,6 +250,8 @@ isValidTimeSeries <- function(
             }   
         ) ) 
         
+        #   Attempt to fix the encoding
+        Encoding( col.Names ) <- 'UTF-8'
         
         # Remove trailing blanks
         col.Names <- sub( 
